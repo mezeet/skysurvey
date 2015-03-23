244ae715e95197bccad831193a93239c28818301
admin <admin@mezeet.com>
2015-03-24 02:49:32 +0900

    슬라이더 양식 활성화 스크립트 추가

--- WebContent/assets/js/scripts.js
+++ WebContent/assets/js/scripts.js
@@ -1,13 +1,15 @@
 
 $(document).ready(function(){/* jQuery toggle layout */
-$('#btnToggle').click(function(){
-  if ($(this).hasClass('on')) {
-    $('#main .col-md-6').addClass('col-md-4').removeClass('col-md-6');
-    $(this).removeClass('on');
-  }
-  else {
-    $('#main .col-md-4').addClass('col-md-6').removeClass('col-md-4');
-    $(this).addClass('on');
-  }
+	$('#btnToggle').click(function(){
+	  if ($(this).hasClass('on')) {
+	    $('#main .col-md-6').addClass('col-md-4').removeClass('col-md-6');
+	    $(this).removeClass('on');
+	  }
+	  else {
+	    $('#main .col-md-4').addClass('col-md-6').removeClass('col-md-4');
+	    $(this).addClass('on');
+	  }
+	});
 });
-});
+
+var mySlider = $("input.slider").slider();

a5385122a28f1557db4098db40f66d42831cf015
mezeet <admin@mezeet.com>
2015-04-07 22:20:58 +0900

    테이블 어노테이션으로 대문자로 된 오라클 테이블 지정

--- src/com/survey/test/model/Member.java
+++ src/com/survey/test/model/Member.java
@@ -1,7 +1,10 @@
 package com.survey.test.model;
 
 import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
 
+
+@Table(value = "MEMBERS")
 public class Member  extends Model{
 	
 }
